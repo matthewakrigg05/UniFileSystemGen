04581538df51a136c52ee25b6976885bf43d09b9
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-04-08 09:31:59 +0100

    feat: adding course context to builder

--- filesystemBuilder.py
+++ filesystemBuilder.py
@@ -1,13 +1,15 @@
 import os
-
 import workingDirectory
+import courseContext
 
 
 class filesystemBuilder:
     directory = None
+    courseInfo = None  # contains all vars that decide how system is structured
 
-    def __init__(self, workingDir: workingDirectory.FileSystemDir):
+    def __init__(self, workingDir: workingDirectory.FileSystemDir, course: courseContext.courseContext):
         self.directory = workingDir
+        self.courseInfo = course
 
     def buildFileSystem(self):
         os.mkdir(self.directory.homeDirectory + "\\University")
